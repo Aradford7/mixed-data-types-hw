f1c013b0eca52e303217184b55fc7c79bddd659f
Ashley Radford <aradford7@outlook.com>
2019-03-25 15:58:19 -0700

    data structures answered

--- hw6.js
+++ hw6.js
@@ -21,13 +21,19 @@ console.log('JS is running');
 
 //Take it Easy
 //Make an array that holds all of the colors of the rainbow.
-
+const Rainbow = ['red','blue','yellow','black','white'];
 //Write code that will access "blue" from the rainbow array.
-
+console.log(Rainbow[1]); //Answer blue
 //Make an object that is called your name and holds the information about your favorite food, 
 //a hobby, the name of the town that you live in currently, and your favorite datatype.
-
+const objectMe = {
+        name:'Ashley',
+        favoritefood: 'EVERYTHING',
+        hobby: 'winning',
+        town: 'Los Angeles',
+        datatype: 'objects',
+};
 //Write code that will access your hobby from the object that you just created.
-
+console.log(objectMe.hobby);
 //Commit: "data structures" Answered
 
