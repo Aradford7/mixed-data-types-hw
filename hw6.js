3e49e86ad992181c0760f15315561d05ae4b1f0f
Ashley Radford <aradford7@outlook.com>
2019-03-25 16:21:13 -0700

    crazyObjects answered

--- hw6.js
+++ hw6.js
@@ -19,21 +19,67 @@ console.log('JS is running');
 //Commit: "data types" Answered
 
 
-//Take it Easy
+//2)Take it Easy
 //Make an array that holds all of the colors of the rainbow.
-const Rainbow = ['red','blue','yellow','black','white'];
+//const Rainbow = ['red','blue','yellow','black','white'];
 //Write code that will access "blue" from the rainbow array.
-console.log(Rainbow[1]); //Answer blue
+//console.log(Rainbow[1]); //Answer blue
 //Make an object that is called your name and holds the information about your favorite food, 
 //a hobby, the name of the town that you live in currently, and your favorite datatype.
+/*
 const objectMe = {
         name:'Ashley',
         favoritefood: 'EVERYTHING',
         hobby: 'winning',
         town: 'Los Angeles',
         datatype: 'objects',
-};
+};*/
 //Write code that will access your hobby from the object that you just created.
-console.log(objectMe.hobby);
+//console.log(objectMe.hobby);
 //Commit: "data structures" Answered
 
+//3) Crazy Object!
+const crazyObject = {
+        taco: [
+          {
+            meat: 'steak',
+            cheese: ['panela', 'queso', 'chihuahua']
+          },
+          {
+            meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+          },
+        ],
+        larry: {
+          nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+          quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+          characters: [
+            {
+              name: "Jeff",
+              occupation: "manager"
+            },
+            {
+              name: "funkhauser",
+              occupation: "tv dude"
+            },
+            {
+              name: "susie",
+              occupation: "jeffs wife",
+              favourtieHobby: "Swearing at Larry and Jeff"
+            },
+          ]
+        }
+      }
+
+//Use crazyObject to log the following.
+//"omg my mouth is burning" // crazyObject.taco.salsa.[5]
+      console.log(crazyObject.taco[1].salsa[5]);
+//"Pretty pretty prettayyyyy good" // crazyObject.larry[1].quotes[0]
+      console.log(crazyObject.larry.quotes[0]);
+//"Swearing at Larry and Jeff"  // crazyObjects.larry.favouriteHobby[0]
+      console.log(crazyObject.larry.characters[2].favourtieHobby);
+//"Chicken Teriyaki Boyyyyyy" // crazyObject.larry.nicknames[1]
+      console.log(crazyObject.larry.nicknames[1]);
+//The object the contains the name funkhauser //crazyObject.larry.characters.[1].name
+      console.log(crazyObject.larry.characters[1].name);
+//Commit: "crazyObject"
