fd373b771ac28c4f32fd9f2027ef53274637bde8
Ashley Radford <aradford7@outlook.com>
2019-03-25 15:50:37 -0700

    Data Types Answered

---
+++ hw6.js
@@ -0,0 +1,33 @@
+console.log('JS is running');
+
+//1) Data Types
+//For each of the following, write which data types you would use to represent the data, 
+//and then give a small example of the data structure:
+//A light switch that can be either on or off.
+        //Booleans
+//A user's email address.
+        //String
+//A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+        //Array
+//A list of student names from our class.
+        //Array and objects
+//A list of student names from our class, each with a location.
+        //Array and objects
+//A list of student names from our class, each with a location and each with a list of 
+//favorite tv shows.
+        //Array and objects
+//Commit: "data types" Answered
+
+
+//Take it Easy
+//Make an array that holds all of the colors of the rainbow.
+
+//Write code that will access "blue" from the rainbow array.
+
+//Make an object that is called your name and holds the information about your favorite food, 
+//a hobby, the name of the town that you live in currently, and your favorite datatype.
+
+//Write code that will access your hobby from the object that you just created.
+
+//Commit: "data structures" Answered
+
