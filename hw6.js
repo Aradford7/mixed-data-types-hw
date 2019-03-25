5b7b6a21c0a260cba4b06295726656c9ecf9f028
Ashley Radford <aradford7@outlook.com>
2019-03-25 16:26:59 -0700

    object-ception answered

--- hw6.js
+++ hw6.js
@@ -83,3 +83,27 @@ const crazyObject = {
 //The object the contains the name funkhauser //crazyObject.larry.characters.[1].name
       console.log(crazyObject.larry.characters[1].name);
 //Commit: "crazyObject"
+
+//4)Object-ception
+//With the following object:
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+//Change the value of limbo to null. //inception.
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
